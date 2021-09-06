9e364cf3aeeb4e6f6a8d8d9bf8b5996c547fc08b
rafael.monteiro <rafael.monteiro@segfy.com>
2021-09-06 07:52:34 -0300

    Created a custom hook to count down timer

--- src/components/PokemonGame/index.tsx
+++ src/components/PokemonGame/index.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import useCountDownTimer from "../../hooks/countDown";
 import PokemonDraw from "./PokemonDraw";
 
 const PokemonGame: React.FC = () => {
+  const { timeLeft, timesUp } = useCountDownTimer(8);
+
   return (
     <>
+      <p>{timesUp ? 'acabou': 'ainda tem'}</p>
+      <progress id="file" value={timeLeft} max="8"></progress>
       <PokemonDraw selectedPokemonId={1} drawSilhouette={true} />
     </>
   );
---
+++ src/hooks/countDown.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+const useCountDownTimer = (seconds: number) => {
+  // initialize timeLeft with the seconds prop
+  const [timeLeft, setTimeLeft] = useState<number>(seconds);
+  const [timesUp, setTimesUp] = useState<boolean>(false);
+
+  useEffect(() => {
+    // exit early when we reach 0
+    if (!timeLeft) {
+      setTimesUp(true);
+      return;
+    }
+
+    // save intervalId to clear the interval when the
+    // component re-renders
+    const intervalId = setInterval(() => {
+      setTimeLeft(timeLeft - 1);
+    }, 1000);
+
+    // clear interval on re-render to avoid memory leaks
+    return () => clearInterval(intervalId);
+    // add timeLeft as a dependency to re-rerun the effect
+    // when we update it
+  }, [timeLeft]);
+
+  return { timeLeft, timesUp };
+};
+
+export default useCountDownTimer;
