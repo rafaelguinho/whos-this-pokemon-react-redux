01952d31feaa6885da0dfefefbd81cadc951bba4
rafael.monteiro <rafael.monteiro@segfy.com>
2021-09-24 08:52:50 -0300

    highlighting the selected option

--- src/components/PokemonGame/PokemonOptions/index.tsx
+++ src/components/PokemonGame/PokemonOptions/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from "react";
+import { PokemonOption } from "../types";
 import { PokemonOptionsProps } from "./types";
 
 const PokemonOptions: React.FC<PokemonOptionsProps> = ({
@@ -6,13 +7,28 @@ const PokemonOptions: React.FC<PokemonOptionsProps> = ({
   isActive,
   optionsClickAction,
 }: PokemonOptionsProps) => {
+  const [selectedItemId, setSelectedItemId] = useState<number | null>(null);
+
+  const selectOption = (option: PokemonOption) => {
+    optionsClickAction(option);
+    setSelectedItemId(option.id);
+  };
+
   if (!options) return <></>;
 
   return (
     <ul>
       {options.map((o) => (
-        <li key={o.id}
-          onClick={(e) => (isActive ? optionsClickAction(o) : function () {})}
+        <li
+          className={
+            selectedItemId
+              ? selectedItemId === o.id
+                ? "highlighter"
+                : "not-highlighter"
+              : ""
+          }
+          key={o.id}
+          onClick={(e) => (isActive ? selectOption(o) : function () {})}
         >
           {o.name}
         </li>
